01befc48cafacf80fee500c56560e622600df224
Amadou B. Jallow <amadoubjallow41@gmail.com>
2026-07-02 00:01:35 +0530

    Added Supabase integration and updated marketplace UI

---
+++ supabase.js
@@ -0,0 +1,14 @@
+const SUPABASE_URL =
+"https://https://qnpaxajpkwlhnjhnijhy.supabase.co/rest/v1/.supabase.co";
+
+const SUPABASE_KEY =
+"sb_publishable_R685ZhPJGUOzX9lFstQ_Fg_N4lHuNRL";
+
+const supabase =
+window.supabase.createClient(
+
+SUPABASE_URL,
+
+SUPABASE_KEY
+
+);
